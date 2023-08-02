8709471331c0c5dd8ae1cbc0d22c0b686ac0e484
Daniel Kennedy <djk2120@columbia.edu>
2023-08-02 15:30:06 -0700

    add refs

--- ms/makebib.sh
+++ ms/makebib.sh
@@ -1,9 +1,10 @@
 grep citation ms.aux | sed 's/citation//g' | tr -d '\\{}' | sort | uniq > cites.txt
 
-d='/Users/kennedy/projects/references/refs/'
+d='/Users/djk2120/projects/references/refs/'
 :> refs.bib
 while read c; do
     file=$d$c".bib"
+    echo $file
     if [ -f $file ]; then
 	cat $file >> refs.bib
 	echo -en '\n' >> refs.bib
